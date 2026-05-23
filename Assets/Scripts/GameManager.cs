282b3cd04192fd3236b060da98c7c3c03bc805b9
ilyafedorov102-blip <ilyafedorov102@gmail.com>
2026-05-23 18:53:01 +0300

    23_05_2026
    18:52

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,48 +1,142 @@
-using Unity.VisualScripting;
+using NUnit.Framework.Interfaces;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject objectPrefab; // Префаб объекта для создания
+    [SerializeField] private GameObject Panel;
+    [SerializeField] private Button btnSetObj;
+    [SerializeField] private float cellSize;
+    [SerializeField] private float zoomSpeed = 5f;
+    [SerializeField] private float minZoom = 2f;
+    [SerializeField] private float maxZoom = 10f;
+    [SerializeField] private float mapWidth = 20f;   // Ширина игрового поля
+    [SerializeField] private float mapHeight = 12f;  // Высота игрового поля
+    [SerializeField] private Color RedCol = Color.red;
+    [SerializeField] private Vector2 minBorder;
+    [SerializeField] private Vector2 maxBorder;
 
-    public GameObject Pause;
-    private bool isPause = false;
+    SpriteRenderer SR;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Color DefaultCol;
+    private GameObject currentObject;
+    private bool isDragging = false;
+
     void Start()
     {
-        
+        // Подписываемся на событие нажатия кнопки
+        btnSetObj.onClick.AddListener(CreateObject);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scroll != 0)
         {
-            isPause = !isPause;
-            Pause.SetActive(isPause);
+            // Сохраняем позицию мыши в мировых координатах
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            // Меняем зум
+            float newZoom = Camera.main.orthographicSize - scroll * zoomSpeed;
+            Camera.main.orthographicSize = Mathf.Clamp(newZoom, minZoom, maxZoom);
+
+            // Смещаем камеру, чтобы мышь оставалась на той же позиции
+            Vector3 newMouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Camera.main.transform.position += mouseWorldPos - newMouseWorldPos;
+            LimitCameraPosition();
+        }
+
+        // Перетаскивание объекта при спавне
+        if (isDragging)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Фиксируем объект на месте
+                isDragging = false;
+                if (currentObject != null)
+                {
+                    SR = currentObject.GetComponent<SpriteRenderer>();
+                    if (SR != null)
+                        SR.color = DefaultCol;
+                }
+                currentObject = null;
+            }
+        }
+
+        if (isDragging && currentObject != null)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.x = Mathf.Round(mousePos.x / cellSize) * cellSize;
+            mousePos.y = Mathf.Round(mousePos.y / cellSize) * cellSize;
+            currentObject.transform.position = mousePos;
+
+            // Проверяем границы и меняем цвет
+            UpdateObjectColor();
         }
     }
 
-    public void Play()
+    void CreateObject()
     {
-        SceneManager.LoadScene("GameWindow"); // для открытия игрового окна
+        // Закрываем панельку и создаем соответствующий ей объект по префабу на месте курсора
+        Panel.SetActive(false);
+
+        if (!isDragging)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.x = Mathf.Round(mousePos.x / cellSize) * cellSize;
+            mousePos.y = Mathf.Round(mousePos.y / cellSize) * cellSize;
+            currentObject = Instantiate(objectPrefab, mousePos, Quaternion.identity);
+
+            // Получаем SpriteRenderer созданного объекта и сохраняем его цвет по умолчанию
+            SR = currentObject.GetComponent<SpriteRenderer>();
+            if (SR != null)
+            {
+                DefaultCol = SR.color;
+            }
+
+            isDragging = true;
+
+            // Проверяем границы сразу после создания
+            UpdateObjectColor();
+        }
     }
 
-    public void Back()
+    void UpdateObjectColor()
     {
-        SceneManager.LoadScene("SampleScene"); // возвращаемся назад в главное меню
+        if (currentObject != null && SR != null)
+        {
+            if (!Obj_in_Borders(currentObject))
+            {
+                SR.color = RedCol;
+            }
+            else
+            {
+                SR.color = DefaultCol;
+            }
+        }
     }
 
-    public void Exit()
+    void LimitCameraPosition()
     {
-        Application.Quit();
-        Debug.Log("Game has been closed");
+        Camera cam = Camera.main;
+        float verticalSize = cam.orthographicSize;
+        float horizontalSize = verticalSize * cam.aspect;
+
+        Vector3 pos = cam.transform.position;
+
+        // Ограничиваем позицию, чтобы камера не выходила за границы карты
+        pos.x = Mathf.Clamp(pos.x, -mapWidth + horizontalSize, mapWidth - horizontalSize);
+        pos.y = Mathf.Clamp(pos.y, -mapHeight + verticalSize, mapHeight - verticalSize);
+
+        cam.transform.position = pos;
     }
 
-    public void ClosePause()
+    bool Obj_in_Borders(GameObject obj)
     {
-        isPause = !isPause;
-        Pause.SetActive(isPause);
+        return (obj.transform.position.x > minBorder.x && obj.transform.position.x < maxBorder.x
+            && obj.transform.position.y > minBorder.y && obj.transform.position.y < maxBorder.y);
     }
 }
